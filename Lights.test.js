a6c05edae833e15b4f13976d13faed1d39b516ef
patrick letourneux <patrick.letourneux20@gmail.com>
2022-08-01 21:58:23 +0200

    refacto test and add global test

--- Lights.test.js
+++ Lights.test.js
@@ -4,7 +4,10 @@ describe('Lights class', () => {
     const Xdimension = 1000;
     const Ydimension = 1000;
     const instanceLights = new Lights(Xdimension,Ydimension);
-    instanceLights.initArrayCoord()
+
+    beforeEach(() => {
+      instanceLights.initArrayCoord();
+    })
 
     describe('countOn()', () => {
       test('countOn() method should return toBeGreaterThanOrEqual(0)', () => {
@@ -16,45 +19,36 @@ describe('Lights class', () => {
 
     describe('initArrayCoord()', () => {
       test('initArrayCoord() method should return an Array', () => {
-        instanceLights.initArrayCoord();
         expect(instanceLights.initArrayCoord()).toEqual(expect.arrayContaining([]));
       });
       test('initArrayCoord() method should return an object .length X*Y', () => {
-        instanceLights.initArrayCoord();
         expect(instanceLights.initArrayCoord()).toHaveLength(Xdimension*Ydimension);
       });
       test("initArrayCoord()[1] toHaveProperty('x','y','ligth')", () => {
-        instanceLights.initArrayCoord();
         expect(instanceLights.initArrayCoord()[0]).toHaveProperty('x','y','ligth');
       });
     });
 
     describe('turnOn()', () => {
       test('turnOn() method should return toBeDefined()', () => {
-        instanceLights.initArrayCoord();
         expect(instanceLights.turnOn(1)).toBeDefined();
       });
       test('turnOn(1,1,1,1) method should return toEqual(1)', () => {
-        instanceLights.initArrayCoord();
         expect(instanceLights.turnOn(1,1,1,1)).toEqual(1);
       });
       test('turnOn(0,0,999,999) method should return toEqual(1000000)', () => {
-        instanceLights.initArrayCoord();
         expect(instanceLights.turnOn(0,0,999,999)).toEqual(1000000);
       });
       test('turnOn(0,0,999,0) method should return toEqual(1000)', () => {
-        instanceLights.initArrayCoord();
         expect(instanceLights.turnOn(0,0,999,0)).toEqual(1000);
       });
     });
 
     describe('turnOff()', () => {
       test('turnOff() method should return toBeDefined()', () => {
-        instanceLights.initArrayCoord();
         expect(instanceLights.turnOff()).toBeDefined();
       });
       test('turnOff(1,1,1,1) method should return toEqual(0)', () => {
-        instanceLights.initArrayCoord();
         instanceLights.turnOn(1,1,1,1)
         expect(instanceLights.turnOff(1,1,1,1)).toEqual(0);
       });  
@@ -62,17 +56,33 @@ describe('Lights class', () => {
 
     describe('toggle()', () => {
       test('toggle() method should return toBeDefined()', () => {
-        instanceLights.initArrayCoord();
         expect(instanceLights.toggle()).toBeDefined();
       });
       test('toggle(1,1,1,1) method should return toEqual(0)', () => {
-        instanceLights.initArrayCoord();
         instanceLights.turnOn(1,1,1,1)
         expect(instanceLights.toggle(1,1,1,1)).toEqual(0);
       });
       test('toggle(1,1,1,1) method should return toEqual(1)', () => {
-        instanceLights.initArrayCoord();
         expect(instanceLights.toggle(1,1,1,1)).toEqual(1);
       });
     });
+    describe('global test', () => {
+      test('global expected 230022', () => {
+        instanceLights.turnOn(887,9,959,629)
+        instanceLights.turnOn(454,398,844,448)
+        
+        instanceLights.turnOff(539,243,559,965)
+        instanceLights.turnOff(370,819,676,868)
+        instanceLights.turnOff(145,40,370,997)
+        instanceLights.turnOff(301,3,808,453)
+        
+        instanceLights.turnOn(351,678,951,908)
+
+        instanceLights.toggle(720,196,897,994)
+        instanceLights.toggle(831,394,904,860)
+
+        expect(instanceLights.countOn()).toEqual(230022);
+        
+      });
+    })
 });
