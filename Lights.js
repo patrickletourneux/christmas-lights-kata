2f8887e3895188964e90bd737b3e02a375beb0d4
patrick letourneux <patrick.letourneux20@gmail.com>
2022-08-01 20:44:02 +0200

    almost done

--- Lights.js
+++ Lights.js
@@ -7,9 +7,48 @@ class Lights {
         this.Ydimension = Ydimension;
     }
     countOn(){
-        return 0
+        let countLight = 0
+        this.arrayCoord.forEach(item => {
+            if (item.light === true){
+                countLight++
+            };
+        })
+        console.log('countLight ',countLight)
+        return countLight
+    }
+    modify(X1,Y1,X2,Y2,action){
+        this.arrayCoord.forEach(item => {
+            if (item.x >= X1 && item.x <= X2 
+                && item.y >= Y1 && item.y <= Y2){
+                    switch (action) {
+                        case "turnOn" :
+                            item.light = true;
+                            break;
+                        case "turnOff" :
+                            item.light = false;
+                            break;
+                        case "toggle" :
+                            item.light = true ? false : true ;
+                            break;
+                    }
+                }
+        })
+        console.log(this.arrayCoord)
+    }
+    turnOn(X1,Y1,X2,Y2){
+        this.modify(X1,Y1,X2,Y2,'turnOn')
+        return this.countOn()
+    }
+    turnOff(X1,Y1,X2,Y2){
+        this.modify(X1,Y1,X2,Y2,'turnOff')
+        return this.countOn()
+    }
+    toggle(X1,Y1,X2,Y2){
+        this.modify(X1,Y1,X2,Y2,'toggle')
+        return this.countOn()
     }
     initArrayCoord(){
+        this.arrayCoord = [];
         // this.arrayCoord = [1];
         for (let i=0;i<this.Xdimension;i++){
             for (let j=0 ; j<this.Ydimension ; j++){
@@ -23,9 +62,6 @@ class Lights {
         }
         console.log(this.arrayCoord)
         return this.arrayCoord
-    }
-    initialize(){
-        this.arrayCoord = [];
     }
 }
 
--- Lights.test.js
+++ Lights.test.js
@@ -4,8 +4,8 @@ const Lights = require('./Lights');
 
 
 describe('Lights class', () => {
-    const Xdimension = 1000;
-    const Ydimension = 1000;
+    const Xdimension = 10;
+    const Ydimension = 10;
     const countCases = Xdimension*Ydimension
     const instanceLights = new Lights(Xdimension,Ydimension);
     instanceLights.initArrayCoord()
@@ -17,12 +17,47 @@ describe('Lights class', () => {
       expect(instanceLights.countOn()).toBeGreaterThanOrEqual(0);
     });
     test('initArrayCoord() method should return an Array', () => {
-      instanceLights.initialize();
+      instanceLights.initArrayCoord();
       expect(instanceLights.initArrayCoord()).toEqual(expect.arrayContaining([]));
     });
     test('initArrayCoord() method should return an object .length X*Y', () => {
-      instanceLights.initialize();
+      instanceLights.initArrayCoord();
       expect(instanceLights.initArrayCoord()).toHaveLength(Xdimension*Ydimension);
+    });
+    test("initArrayCoord()[1] toHaveProperty('x','y','ligth')", () => {
+      instanceLights.initArrayCoord();
+      expect(instanceLights.initArrayCoord()[0]).toHaveProperty('x','y','ligth');
+    });
+    test('turnOn() method should return toBeDefined()', () => {
+      instanceLights.initArrayCoord();
+      expect(instanceLights.turnOn(1)).toBeDefined();
+    });
+    test('turnOff() method should return toBeDefined()', () => {
+      instanceLights.initArrayCoord();
+      expect(instanceLights.turnOff()).toBeDefined();
+    });
+    test('toggle() method should return toBeDefined()', () => {
+      instanceLights.initArrayCoord();
+      expect(instanceLights.toggle()).toBeDefined();
+    });
+    test('turnOn(1,1,1,1) method should return toEqual(1)', () => {
+      instanceLights.initArrayCoord();
+      expect(instanceLights.turnOn(1,1,1,1)).toEqual(1);
+    });
+    test('turnOff(1,1,1,1) method should return toEqual(0)', () => {
+      instanceLights.initArrayCoord();
+      instanceLights.turnOn(1,1,1,1)
+      expect(instanceLights.turnOff(1,1,1,1)).toEqual(0);
+    });
+    test('toggle(1,1,1,1) method should return toEqual(0)', () => {
+      instanceLights.initArrayCoord();
+      instanceLights.turnOn(1,1,1,1)
+      expect(instanceLights.toggle(1,1,1,1)).toEqual(0);
+    });
+    test('toggle(1,1,1,1) method should return toEqual(8)', () => {
+      instanceLights.initArrayCoord();
+      instanceLights.turnOn(1,1,3,3)
+      expect(instanceLights.toggle(1,1,1,1)).toEqual(8);
     });
 
 
